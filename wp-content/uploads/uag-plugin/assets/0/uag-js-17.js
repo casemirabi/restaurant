e5e7e8023b50134523c9f9f8375db992bd4c4e08
casemirabi <biancacaasemira@gmail.com>
2025-09-01 22:40:50 -0300

    Site estático de restaurante

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-17.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"2ad7626b","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-2ad7626b' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"2ad7626b","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-2ad7626b' );
+});
+ });
